01e42f518c30e12152ce438c47f81441e4c4512f
Neoman95 <kchwacinski@edu.cdv.pl>
2025-01-09 17:22:59 +0100

    Consumer's product

--- main/consumer.py
+++ main/consumer.py
@@ -1,4 +1,8 @@
+import json
+
 import pika
+
+from main import Product, db
 
 params = pika.URLParameters('amqps://yfunjwyl:sQec8gfKBkduocgGxh_PY8ajSuXimivU@seal.lmq.cloudamqp.com/yfunjwyl')
 connection = pika.BlockingConnection(params)
@@ -9,9 +13,29 @@ channel.queue_declare(queue='main')
 
 def callback(ch, method, properties, body):
     print('Received in admin')
-    print(body)
+    data = json.loads(body)
+    print(data)
 
-channel.basic_consume(queue='main', on_message_callback=callback)
+    if properties.content_type == 'product_created':
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+        print('Product created')
+
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+        print('Product updated')
+
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+        print('Product deleted')
+
+channel.basic_consume(queue='main', on_message_callback=callback,auto_ack=True)
 
 print('Started consuming')
 channel.start_consuming()
